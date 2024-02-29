3f07ba2a66dd24a93569387a54dee80931201fcb
Danilodevald123 <danilo.devald18@gmail.com>
2024-02-29 13:55:27 -0300

    all changes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,18 +6,17 @@ const Hero = () => {
   return (
    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
     <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-    <p>
+    <p className="text-xl font-montserrat text-coral-red">
       Out Summer Collection
     </p>
-    <h1>
-      <span>
+    <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+      <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
         The New Arrival
       </span>
       <br/>
-      <span>Nike </span>
-      Shoes
+      <span className="text-coral-red inline-block mt-3">Nike </span> Shoes
     </h1>
-    <p>
+    <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6  mb-14 sm:max-w-sm">
       Discover stylish Nike arrivals, quality comfort, and innovation for your active life.
     </p>
       <Button label="Shop Now" iconURL={arrowRight}/>
@@ -25,8 +24,8 @@ const Hero = () => {
       {statistics.map((stat) => 
       (
         <div key={stat.label}>
-        <p>{stat.value}</p>
-        <p>{stat.label}</p>
+        <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+        <p className="leading-7 font-montserrat text-slate-gray" >{stat.label}</p>
         </div>
       ))}
     </div>  
