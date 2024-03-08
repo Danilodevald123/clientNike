192703d41ac5dc21e807952669c72e10825f9fd8
Danilodevald123 <danilo.devald18@gmail.com>
2024-03-08 13:53:45 -0300

    add shoes img

---
+++ src/components/ShoeCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShoeCard = ({imgURL, changeBigShoeImage, bigShoeImage}) => {
+  return (
+    <div className={`border-2 rounded-xl ${bigShoeImage === imgURL ? `border-coral-red` : `border-transparent` }`}>
+    ShoeCard
+    </div>
+  )
+}
+
+export default ShoeCard
